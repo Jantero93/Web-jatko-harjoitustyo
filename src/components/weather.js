4865f4980fa870a6257456d2736a682ed96fbf67
Jantero93 <janzku1993@hotmail.com>
2020-11-23 08:27:46 +0200

    valmis

--- src/components/weather.js
+++ src/components/weather.js
@@ -15,53 +15,50 @@ export default class Weather extends Component {
             description: "",
             sunrise: null,
             sunset: null,
-            color: 'white',
-           
+            color: '',
+
         };
-     //   this.getWeather();
+        //   this.getWeather();
 
     }
     // ehkä oikea https://api.openweathermap.org/data/2.5/weather?id=32906&appid=881e2e4957f0b379389af22826a33532&units=metric
     // siilinjärvi  https://api.openweathermap.org/data/2.5/weather?id=636947&appid=881e2e4957f0b379389af22826a33532&units=metric
-    getWeather = async () => {
+    getWeather = async (color) => {
         const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=634964&appid=881e2e4957f0b379389af22826a33532&units=metric`)
         const response = await api_call.json();
-    
+
         let today = new Date();
         let sunset = response.sys.sunset;
         let sunrise = response.sys.sunrise;
 
-       console.log("auringonnousu",response.sys.sunrise);
-
+        console.log("auringonnousu", response.sys.sunrise);
+        
+        //aika systeemistä
         let time;
         if (today.getMinutes() < 10) {
             time = today.getHours() + ":" + '0' + today.getMinutes();
         } else {
             time = today.getHours() + ":" + today.getMinutes();
         }
+
         // unix aika oikeaksi ajaksi, auringonlasku
         today = new Date(1000 * sunset);
-
         if (today.getMinutes() < 10) {
             sunset = today.getHours() + ":" + '0' + today.getMinutes();
         } else {
             sunset = today.getHours() + ":" + today.getMinutes();
         }
 
-
-
-       
+        //unix aika oikeaksi ajaksi, auringonnousu
         today = new Date(1000 * sunrise);
-
         if (today.getMinutes() < 10) {
             sunrise = today.getHours() + ":" + '0' + today.getMinutes();
         } else {
             sunrise = today.getHours() + ":" + today.getMinutes();
         }
 
-        //sunrise = today.getHours() + ":" + today.getMinutes();
-
-        //console.log('lampotila: ',this.temp)
+        //tekstin väri, parametrina
+        color = "white";
 
         this.setState({
             time: time,
@@ -72,141 +69,142 @@ export default class Weather extends Component {
             description: response.weather[0].description,
             sunrise: sunrise,
             sunset: sunset,
-            color: 'white',
-           
+            color: color
         });
     }
-  
-    
-    componentDidMount(){
+
+
+    componentDidMount() {
         this.getWeather();
     }
 
     render() {
         return (
             <div className="website">
-            <Container
-            fluid="true"
-            style={{marginLeft: "1rem"}}           
-            >
+                <Container
+                    fluid="true"
+                    style={{ marginLeft: "1rem" }}
+                >
 
-                <Row>
-                    {/*Aika*/}
-                    <Card
-                        bg='secondary'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>Time</Card.Header>
-                        <Card.Body>
-                            <Card.Title>{this.state.time} </Card.Title>
-                        </Card.Body>
-                    </Card>
-
-
-
-                    {/* Lämpötila */}
-                    <Card
-                        bg='danger'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>Temperature</Card.Header>
-                        <Card.Body>
-                            <Card.Title> {Math.round(this.state.temp * 10) / 10} celsius </Card.Title>
-                        </Card.Body>
-                    </Card>
+                    <Row>
+                        {/*Aika*/}
+                        <Card
+                            bg='secondary'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>Time</Card.Header>
+                            <Card.Body>
+                                <Card.Title>{this.state.time} </Card.Title>
+                            </Card.Body>
+                        </Card>
 
 
-                    <Card
-                        bg='info'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>Humidity</Card.Header>
-                        <Card.Body>
-                            <Card.Title>{this.state.humidity} % </Card.Title>
-                        </Card.Body>
-                    </Card>
 
-                    {/*Auringon nousu*/}
-                    <Card
-                        bg='success'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>Sunrise</Card.Header>
-                        <Card.Body>
-                            <Card.Title> {this.state.sunrise}</Card.Title>
-                        </Card.Body>
-                    </Card>
-
-                </Row>
-
-                <Row>
-
-                    {/*Paikka*/}
-                    <Card
-                        bg='secondary'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>City</Card.Header>
-                        <Card.Body>
-                            <Card.Title>{this.state.place} </Card.Title>
-                        </Card.Body>
-                    </Card>
+                        {/* Lämpötila */}
+                        <Card
+                            bg='danger'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>Temperature</Card.Header>
+                            <Card.Body>
+                                <Card.Title> {Math.round(this.state.temp * 10) / 10} celsius </Card.Title>
+                            </Card.Body>
+                        </Card>
 
 
-                    {/* Miltä tuntuu */}
-                    <Card
-                        bg='danger'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>Feels like</Card.Header>
-                        <Card.Body>
-                            <Card.Title> {Math.round(this.state.feels_like * 10) / 10} celsius</Card.Title>
-                        </Card.Body>
-                    </Card>
+                        <Card
+                            bg='info'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>Humidity</Card.Header>
+                            <Card.Body>
+                                <Card.Title>{this.state.humidity} % </Card.Title>
+                            </Card.Body>
+                        </Card>
 
-                    {/*Kuvaus*/}
-                    <Card
-                        bg='info'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>Description</Card.Header>
-                        <Card.Body>
-                            <Card.Title>{this.state.description}  </Card.Title>
-                        </Card.Body>
-                    </Card>
+                        {/*Auringon nousu*/}
+                        <Card
+                            bg='success'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>Sunrise</Card.Header>
+                            <Card.Body>
+                                <Card.Title> {this.state.sunrise}</Card.Title>
+                            </Card.Body>
+                        </Card>
 
-                    {/* Auringonlasku */}
-                    <Card
-                        bg='success'
-                        text={this.state.color}
-                        style={{ width: '15rem' }}
-                        className="m-1"
-                    >
-                        <Card.Header>Sunset</Card.Header>
-                        <Card.Body>
-                            <Card.Title> {this.state.sunset}</Card.Title>
-                        </Card.Body>
-                    </Card>
+                    </Row>
 
-                </Row>
-            </Container>
+                    <Row>
+
+                        {/*Paikka*/}
+                        <Card
+                            bg='secondary'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>City</Card.Header>
+                            <Card.Body>
+                                <Card.Title>{this.state.place} </Card.Title>
+                            </Card.Body>
+                        </Card>
+
+
+                        {/* Miltä tuntuu */}
+                        <Card
+                            bg='danger'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>Feels like</Card.Header>
+                            <Card.Body>
+                                <Card.Title> {Math.round(this.state.feels_like * 10) / 10} celsius</Card.Title>
+                            </Card.Body>
+                        </Card>
+
+                        {/*Kuvaus*/}
+                        <Card
+                            bg='info'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>Description</Card.Header>
+                            <Card.Body>
+                                <Card.Title>{this.state.description}  </Card.Title>
+                            </Card.Body>
+                        </Card>
+
+                        {/* Auringonlasku */}
+                        <Card
+                            bg='success'
+                            text={this.state.color}
+                            style={{ width: '15rem' }}
+                            className="m-1"
+                        >
+                            <Card.Header>Sunset</Card.Header>
+                            <Card.Body>
+                                <Card.Title> {this.state.sunset}</Card.Title>
+                            </Card.Body>
+                        </Card>
+
+                    </Row>
+                    
+                </Container>
+               
             </div>
+            
         );
     }
 
 }
-
