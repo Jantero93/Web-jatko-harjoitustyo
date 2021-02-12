090082fa73ab789c11164649d65e0ec5ea783f3a
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2021-02-12 14:05:35 +0200

    hieman siistimistä koodissa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import React, { Component } from "react";
-import { Switch, Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav} from 'react-bootstrap';
-import Navbar from 'react-bootstrap/Navbar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
 
 import AddNote from "./components/add-note.component";
 import NotesList from "./components/note-list.component";
@@ -16,32 +15,29 @@ import Weather from "./components/weather";
 class App extends Component {
   render() {
     return (
-      
       <div className="website">
-
         <Navbar bg="dark" expand="sm" text="white">
-
-
-          <Navbar.Brand href="/"><p1
-            style={{ color: "goldenrod" }}
-          >
-            <strong> Home</strong>
-          </p1></Navbar.Brand>
-
+          <Navbar.Brand href="/">
+            <p style={{ color: "goldenrod" }}>
+              <strong> Home</strong>
+            </p>
+          </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-
-              <Nav.Link href="/messages"><p1 style={{ color: "white" }}>Messages</p1></Nav.Link>
-              <Nav.Link href="/add"><p1 style={{ color: "white" }}>Add message</p1></Nav.Link>
-              <Nav.Link href="/weather"><p1 style={{ color: "white" }}>Weather Station</p1></Nav.Link>
-
+              <Nav.Link href="/messages">
+                <p style={{ color: "white" }}>Messages</p>
+              </Nav.Link>
+              <Nav.Link href="/add">
+                <p style={{ color: "white" }}>Add message</p>
+              </Nav.Link>
+              <Nav.Link href="/weather">
+                <p style={{ color: "white" }}>Weather Station</p>
+              </Nav.Link>
             </Nav>
-
           </Navbar.Collapse>
         </Navbar>
-       
 
         <Switch>
           <Route exact path="/" component={Home} />
@@ -49,13 +45,9 @@ class App extends Component {
           <Route exact path="/add" component={AddNote} />
           <Route exact path="/weather" component={Weather} />
         </Switch>
-
-
       </div>
     );
   }
 }
 
-
 export default App;
-
